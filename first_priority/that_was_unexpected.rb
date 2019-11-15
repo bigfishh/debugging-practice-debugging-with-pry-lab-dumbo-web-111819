0cb85cc8091a016f5e294571b30a060c8f006cd4
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-15 01:45:13 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,6 +1,9 @@
+require_relative '../first_priority/that_was_unexpected'
+require 'pry'
+
 class Person
   attr_accessor :name, :candy_lover
-
+  
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
@@ -24,6 +27,7 @@ class Person
     else
       10.times do
         "not a ballin candy lover"
+      end
     end
   end
 end
